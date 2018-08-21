e3f4a0ccde9866bb8c205358c5b72a67472082fc
MrTsao <rcode@163.com>
2018-08-21 10:42:15 +0800

    init

---
+++ StlPDFConvert/PDFConvert.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace StlPDFConvert
+{
+    partial class PDFConvert
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDFConvert));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnOutFileView = new System.Windows.Forms.Button();
+            this.btnInFileView = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstIn = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsst = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtWidth);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.btnOutFileView);
+            this.groupBox1.Controls.Add(this.btnInFileView);
+            this.groupBox1.Controls.Add(this.txtOut);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtIn);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(649, 78);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(487, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "px";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(454, 50);
+            this.txtWidth.MaxLength = 3;
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(31, 21);
+            this.txtWidth.TabIndex = 10;
+            this.txtWidth.Text = "300";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(452, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "单页宽度";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(580, 18);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(52, 55);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(521, 18);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(52, 55);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnOutFileView
+            // 
+            this.btnOutFileView.Location = new System.Drawing.Point(355, 50);
+            this.btnOutFileView.Name = "btnOutFileView";
+            this.btnOutFileView.Size = new System.Drawing.Size(75, 23);
+            this.btnOutFileView.TabIndex = 6;
+            this.btnOutFileView.Text = "浏览";
+            this.btnOutFileView.UseVisualStyleBackColor = true;
+            this.btnOutFileView.Click += new System.EventHandler(this.btnOutFileView_Click);
+            // 
+            // btnInFileView
+            // 
+            this.btnInFileView.Location = new System.Drawing.Point(355, 18);
+            this.btnInFileView.Name = "btnInFileView";
+            this.btnInFileView.Size = new System.Drawing.Size(75, 23);
+            this.btnInFileView.TabIndex = 5;
+            this.btnInFileView.Text = "浏览";
+            this.btnInFileView.UseVisualStyleBackColor = true;
+            this.btnInFileView.Click += new System.EventHandler(this.btnInFileView_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(66, 52);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(283, 21);
+            this.txtOut.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "输出目录";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "监视目录";
+            // 
+            // txtIn
+            // 
+            this.txtIn.Location = new System.Drawing.Point(66, 20);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(283, 21);
+            this.txtIn.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 78);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(649, 300);
+            this.splitContainer1.SplitterDistance = 307;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstIn);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(307, 300);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "待转换文件";
+            // 
+            // lstIn
+            // 
+            this.lstIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstIn.FormattingEnabled = true;
+            this.lstIn.ItemHeight = 12;
+            this.lstIn.Location = new System.Drawing.Point(3, 17);
+            this.lstIn.Name = "lstIn";
+            this.lstIn.Size = new System.Drawing.Size(301, 280);
+            this.lstIn.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lstOut);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(338, 300);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "已转换文件";
+            // 
+            // lstOut
+            // 
+            this.lstOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.ItemHeight = 12;
+            this.lstOut.Location = new System.Drawing.Point(3, 17);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(332, 280);
+            this.lstOut.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsst});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 378);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(649, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsst
+            // 
+            this.tsst.Name = "tsst";
+            this.tsst.Size = new System.Drawing.Size(32, 17);
+            this.tsst.Text = "就绪";
+            // 
+            // PDFConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 400);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PDFConvert";
+            this.Text = "PDF 缩略图转换";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PDFConvert_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOutFileView;
+        private System.Windows.Forms.Button btnInFileView;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstIn;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox lstOut;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsst;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StlPDFConvert/PDFConvert.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Permissions;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Configuration;
+
+namespace StlPDFConvert
+{
+    public partial class PDFConvert : Form
+    {
+        public static string OutPath = "";
+        public static string InPath = "";
+
+        public static PDFConvert mainFrm;
+
+        public static int iWidth = 300;
+
+        public static FileSystemWatcher watcher = new FileSystemWatcher();
+        public static Dictionary<string, string> files;
+        AppSettingsReader appSettings = new AppSettingsReader();
+
+        /// <summary>
+        /// 打印线程队列
+        /// </summary>
+        Thread ConvertThread = null;
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public PDFConvert()
+        {
+            mainFrm = this;
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+
+            SetStatus(false);
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            InPath = config.AppSettings.Settings["pdfInputPath"].Value;
+            OutPath = config.AppSettings.Settings["pngOutputPath"].Value;
+            this.txtIn.Text = InPath;
+            this.txtOut.Text = OutPath;
+            //appSettings.GetValue("pngOutputPath", typeof(string)).ToString();
+        }
+
+        private void btnInFileView_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.txtIn.Text = fbd.SelectedPath;
+                string InPath = fbd.SelectedPath;
+                SetConfigValue("pdfInputPath", fbd.SelectedPath);
+            }
+        }
+
+        private void btnOutFileView_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.txtOut.Text = fbd.SelectedPath;
+                SetConfigValue("pngOutputPath", fbd.SelectedPath);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            SetStatus(true);
+            OutPath = this.txtOut.Text;
+            InPath = this.txtIn.Text;
+            files = new Dictionary<string, string>();
+            Int32.TryParse(this.txtWidth.Text, out iWidth);
+
+            if (string.IsNullOrEmpty(OutPath) || string.IsNullOrEmpty(InPath) || iWidth < 100)
+            {
+                MessageBox.Show("目录为空，单页宽度不能小于100!");
+                return;
+            }
+
+            ConvertThread = new Thread(new ParameterizedThreadStart(Start));
+            ConvertThread.SetApartmentState(ApartmentState.STA);//.ApartmentState = ApartmentState.STA;
+            ConvertThread.Start(this.txtIn.Text);
+        }
+
+
+        private static void Start(object obj)
+        {
+            watcher.Path = obj.ToString();
+            watcher.Filter = "*.pdf";
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+           | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        // Define the event handlers.
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            //watcher.EnableRaisingEvents = false;
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            //Convert cv = new Convert();
+            //cv.ToConvert(e.FullPath, OutPath);
+            if (e.ChangeType != WatcherChangeTypes.Deleted)
+            {
+                string filepath = e.FullPath; //mypath + "//" + cr.Name;
+                FileInfo fi = new FileInfo(filepath);
+
+                if (!fi.Exists)
+                {
+                    Console.WriteLine("file not exits!!");
+                }
+
+            HELLO: try
+                {
+
+                    fi.OpenRead().Close();
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(1000);
+                    goto HELLO;
+                }
+
+
+                if (!files.ContainsKey(fi.Name))
+                {
+                    mainFrm.tsst.Text = "开始转换" + fi.Name + "！";
+                    files.Add(fi.Name, fi.Name);
+                    mainFrm.lstIn.Items.Add(fi.Name);
+                    Thread staThread = new Thread(new ParameterizedThreadStart(ToConvert));
+                    staThread.SetApartmentState(ApartmentState.STA);//.ApartmentState = ApartmentState.STA;
+                    staThread.Start(e.FullPath);
+                }
+            }
+
+            //watcher.EnableRaisingEvents = true;
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+
+        }
+
+        public static void ToConvert(object strInFilePath)
+        {
+            Acrobat.CAcroPDDoc pdfDoc;
+            Acrobat.CAcroPDPage pdfPage;
+            Acrobat.CAcroRect pdfRect;
+            Acrobat.CAcroPoint pdfPoint;
+
+            pdfDoc = (Acrobat.CAcroPDDoc)Microsoft.VisualBasic.Interaction.CreateObject("AcroExch.PDDoc", "");
+            bool ret = pdfDoc.Open(strInFilePath.ToString());
+
+            if (!ret)
+            {
+                return;
+                //throw new FileNotFoundException();
+            }
+
+            string strOriFileName = strInFilePath.ToString().Substring(strInFilePath.ToString().LastIndexOf(@"\") + 1);
+            string strFileName = strOriFileName.Replace(".pdf", ".png");
+            string outputFile = OutPath + "\\" + strFileName;
+
+            int pageCount = pdfDoc.GetNumPages();
+
+            pdfPage = (Acrobat.CAcroPDPage)pdfDoc.AcquirePage(0);
+            pdfPoint = (Acrobat.CAcroPoint)pdfPage.GetSize();
+
+            pdfRect = (Acrobat.CAcroRect)Microsoft.VisualBasic.Interaction.CreateObject("AcroExch.Rect", "");
+
+            pdfRect.Left = 0;
+            pdfRect.right = pdfPoint.x;
+            pdfRect.Top = 0;
+            pdfRect.bottom = pdfPoint.y;
+
+            int thumbnailWidth = iWidth;
+            int thumbnailHeight = (int)(thumbnailWidth * (pdfPoint.y * 1.0) / pdfPoint.x);
+
+            double rate = (pdfPoint.y * 1.0) / pdfPoint.x;
+            int imgwidth = 3 * thumbnailWidth + 30;
+            int imgHeight = (int)(Math.Ceiling((pageCount > 9 ? 9 : pageCount) / 3.0) * thumbnailHeight) + 30;
+
+            Bitmap thumbnailBitmap = new Bitmap(imgwidth, imgHeight,
+                                               System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+
+            for (int i = 0; i < pageCount && i < 9; i++)
+            {
+                // Get the first page
+                pdfPage = (Acrobat.CAcroPDPage)pdfDoc.AcquirePage(i);
+                // Render to clipboard, scaled by 100 percent (ie. original size)
+                // Even though we want a smaller image, better for us to scale in .NET
+                // than Acrobat as it would greek out small text
+                // see http://www.adobe.com/support/techdocs/1dd72.htm
+                pdfPage.CopyToClipboard(pdfRect, 0, 0, 100);
+
+                IDataObject clipboardData = Clipboard.GetDataObject();
+
+                if (clipboardData.GetDataPresent(DataFormats.Bitmap))
+                {
+                    Bitmap pdfBitmap = (Bitmap)clipboardData.GetData(DataFormats.Bitmap);
+
+                    // Render to small image using the bitmap class
+                    Image pdfImage = pdfBitmap.GetThumbnailImage(thumbnailWidth, thumbnailHeight,
+                                                                 null, IntPtr.Zero);
+
+                    using (Graphics thumbnailGraphics = Graphics.FromImage(thumbnailBitmap))
+                    {
+                        // Draw rendered pdf image to new blank bitmap
+                        thumbnailGraphics.DrawImage(pdfImage, i % 3 * (thumbnailWidth + 2), i / 3 * (thumbnailHeight + 2), thumbnailWidth, thumbnailHeight);
+                        thumbnailGraphics.Save();
+                    }
+
+                }
+
+            }
+            pdfDoc.Close();
+            Marshal.ReleaseComObject(pdfPage);
+            Marshal.ReleaseComObject(pdfRect);
+            Marshal.ReleaseComObject(pdfDoc);
+            thumbnailBitmap.Save(outputFile);
+            thumbnailBitmap.Dispose();
+
+            mainFrm.lstIn.Items.Remove(strOriFileName);
+            mainFrm.lstOut.Items.Add(strFileName);
+            if (files.ContainsKey(strOriFileName))
+            {
+                files.Remove(strOriFileName);
+            }
+            mainFrm.tsst.Text = strFileName + "  转换完成！";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (ConvertThread.IsAlive)
+            {
+                ConvertThread.Join(100);
+            }
+
+            watcher.EnableRaisingEvents = false;
+
+            SetStatus(false);
+        }
+
+
+        //private void AutoRun_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    if (AutoRun.Checked)//开机自动启动
+        //    {
+        //        try
+        //        {
+        //            RegistryKey runKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+        //            runKey.SetValue("文件夹监测.exe", System.Windows.Forms.Application.ExecutablePath);
+        //            runKey.Close();
+        //        }
+        //        catch (IOException)
+        //        {
+        //            return;
+        //        }
+        //    }
+        //    else  //不开机自动启动注册表信息删除
+        //    {
+        //        RegistryKey software = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+        //        string[] aimnames = software.GetValueNames();
+        //        foreach (string aimKey in aimnames)
+        //        {
+        //            if (aimKey.Equals("文件夹监测.exe"))
+        //            {
+        //                software.DeleteValue("文件夹监测.exe");
+        //                software.Close();
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        private void SetStatus(bool isTrans)
+        {
+            this.txtIn.Enabled = !isTrans;
+            this.txtOut.Enabled = !isTrans;
+            this.txtWidth.Enabled = !isTrans;
+            this.btnStart.Enabled = !isTrans;
+            this.btnInFileView.Enabled = !isTrans;
+            this.btnOutFileView.Enabled = !isTrans;
+            this.btnStop.Enabled = isTrans;
+        }
+
+
+        /// <summary>
+        /// 修改AppSettings中配置
+        /// </summary>
+        /// <param name="key">key值</param>
+        /// <param name="value">相应值</param>
+        public static bool SetConfigValue(string key, string value)
+        {
+            try
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                if (config.AppSettings.Settings[key] != null)
+                    config.AppSettings.Settings[key].Value = value;
+                else
+                    config.AppSettings.Settings.Add(key, value);
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void PDFConvert_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+
+    }
+}
